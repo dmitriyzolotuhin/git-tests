5e74732b7e3b1a6bf0b7be44e72d6b299a5c496c
Victor Visockiy <victorvisockiy@noorsoft.ru>
2021-04-14 00:34:45 +0300

    Incorrect commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const resultElem = document.querySelector('#result');
 const buttonElem = document.querySelector('#btn');
 
-const generateRandomNumber = () => Math.round(1000 * Math.random());
+const generateRandomNumber = () => Math.round(0 * Math.random());
 
 buttonElem.addEventListener("click", () => {
     const result = generateRandomNumber();
